d025d8ee5094c4381ac37e1ba1e905d9774fccd0
= <j.shrimpton@uckfield.college>
2019-03-20 20:32:18 +0000

    made projectiles directional

--- Fireball.gd
+++ Fireball.gd
@@ -3,14 +3,23 @@ extends Area2D
 const SPEED = 100
 
 var velocity = Vector2()
+var direction = 1
 
 func _ready():
 	pass # Replace with function body.
 
+func set_fireball_direction(dir):
+	direction = dir
+	if dir == -1:
+		$AnimatedSprite.flip_h = true
+
 func _physics_process(delta):
-	velocity.x = SPEED * delta
+	velocity.x = SPEED * delta * direction
 	translate(velocity)
 	$AnimatedSprite.play("shoot")
 
 func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+func _on_Fireball_body_entered(body):
 	queue_free()
--- Player.gd
+++ Player.gd
@@ -15,10 +15,16 @@ func _physics_process(delta):
 		velocity.x = SPEED
 		$AnimatedSprite.play("run")
 		$AnimatedSprite.flip_h = false
+		if sign($Position2D.position.x) == -1:
+			$Position2D.position.x *= -1
+			
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
 		$AnimatedSprite.play("run")
 		$AnimatedSprite.flip_h = true
+		if sign($Position2D.position.x) == 1:
+			$Position2D.position.x *= -1
+		
 	else:
 		velocity.x = 0
 		$AnimatedSprite.play("idle")
@@ -28,9 +34,12 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("ui_select"):
 		var fireball = FIREBALL.instance()
+		if sign($Position2D.position.x) == 1:
+			fireball.set_fireball_direction(1)
+		else:
+			fireball.set_fireball_direction(-1)
 		get_parent().add_child(fireball)
 		fireball.position = $Position2D.global_position
-		$AnimatedSprite.play("shoot")
 	
 	velocity.y += GRAVITY
 	
